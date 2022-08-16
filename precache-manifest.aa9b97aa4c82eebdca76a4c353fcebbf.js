6c4f07d000eee1e9e6e07fc62e1574bfbcd88369
runa-shuak <63771796+runa-shuak@users.noreply.github.com>
2022-08-16 18:20:50 +0900

    Add files via upload

---
+++ precache-manifest.aa9b97aa4c82eebdca76a4c353fcebbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d89f8cd050c223a2537",
+    "url": "/web/static/css/main.c7422e1b.chunk.css"
+  },
+  {
+    "revision": "0d89f8cd050c223a2537",
+    "url": "/web/static/js/main.0d89f8cd.chunk.js"
+  },
+  {
+    "revision": "f2da7e30273ead0a4d03",
+    "url": "/web/static/css/1.52a7850f.chunk.css"
+  },
+  {
+    "revision": "f2da7e30273ead0a4d03",
+    "url": "/web/static/js/1.f2da7e30.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "e49bc305791c67b67f4dd32712801d4d",
+    "url": "/web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d4d552db2a3b0706731e1af992a3e9c.js"
+  "/web/precache-manifest.aa9b97aa4c82eebdca76a4c353fcebbf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/web/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
