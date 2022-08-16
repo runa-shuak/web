36f4a6b5c68864eb9c0c34dd6f55d3633c38e761
runa-shuak <63771796+runa-shuak@users.noreply.github.com>
2022-08-16 17:38:19 +0900

    Add files via upload

---
+++ precache-manifest.5d4d552db2a3b0706731e1af992a3e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62e48b6c880540ccac6d",
+    "url": "/static/css/main.4e9d9744.chunk.css"
+  },
+  {
+    "revision": "62e48b6c880540ccac6d",
+    "url": "/static/js/main.62e48b6c.chunk.js"
+  },
+  {
+    "revision": "39f30f55a77ce661e88c",
+    "url": "/static/css/1.be4bf11a.chunk.css"
+  },
+  {
+    "revision": "39f30f55a77ce661e88c",
+    "url": "/static/js/1.39f30f55.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7934da2a1cd1763be20e1d2bd3e82bb",
+    "url": "/index.html"
+  }
+];
